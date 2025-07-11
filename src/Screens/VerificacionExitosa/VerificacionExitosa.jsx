00e4e439b19ce92dbec089ac0d6826ee0f396abd
juan sandez <sandezjuan@gmail.com>
2025-07-11 02:00:37 -0300

    actualizacion boton login

--- src/Screens/VerificacionExitosa/VerificacionExitosa.jsx
+++ src/Screens/VerificacionExitosa/VerificacionExitosa.jsx
@@ -6,7 +6,7 @@ const VerificacionExitosa = () => (
     <h1>¡Listo!</h1>
     <h3>Tu cuenta ha sido verificada!</h3>
     <p>Ya podés iniciar sesión.</p>
-    <Link to="/login">Ir al login</Link>
+    <Link to="/login" className="boton-login">Ir al login</Link>
   </div>
 );
 export default VerificacionExitosa
